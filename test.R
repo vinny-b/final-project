72cc44b5e6a786404f6fbcabdf3f74dce3d6eba0
Anish Velagapudi <16AnishV@gmail.com>
2016-11-30 16:28:11 -0800

    Did some testing with the Twitter API and twitteR package

---
+++ test.R
@@ -0,0 +1,23 @@
+install.packages('twitteR')
+library(twitteR)
+
+if (!require("twitteR")) {
+  install.packages("twitteR", repos="http://cran.rstudio.com/") 
+  library("twitteR")
+}
+
+consumer.key <- '974S5E5vr0pJ8VuOx2oIYf4Z0'
+consumer.secret <- '8acGBD7TKoLhFVG5TSsfa0PMeUpwnQgQVDwL1BfoGy5XHowIMb'
+
+access.token <- '804111108076228608-K6ml4zNsg2RZLb4ep3Dir4JJhuzb1AQ'
+access.secret <- 'nwvP4yzA6mKNE2P0Ma0diEvHQNN24McUnLH80kbmD4uGx'
+options(httr_oauth_cache=T) #This will enable the use of a local file to cache OAuth access credentials between R sessions.
+setup_twitter_oauth(consumer.key, consumer.secret, access.token, access.secret)
+
+lucaspuente <- getUser("lucaspuente")
+location(lucaspuente)
+
+lucaspuente_follower_IDs<-lucaspuente$getFollowers(retryOnRateLimit=180)
+length(lucaspuente_follower_IDs)
+
+favorites("lucaspuente", n = 20)
