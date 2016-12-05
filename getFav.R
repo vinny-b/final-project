45a89d15b948269e38c91d11eba5657582f46466
Katelin Kobuke <Kate@D-173-250-194-210.dhcp4.washington.edu>
2016-12-04 17:00:05 -0800

    added getFav file

---
+++ getFav.R
@@ -0,0 +1,59 @@
+# install.packages('twitteR')
+library(twitteR)
+library(dplyr)
+
+if (!require("twitteR")) {
+  install.packages("twitteR", repos="http://cran.rstudio.com/") 
+  library("twitteR")
+}
+
+consumer.key <- '974S5E5vr0pJ8VuOx2oIYf4Z0'
+consumer.secret <- '8acGBD7TKoLhFVG5TSsfa0PMeUpwnQgQVDwL1BfoGy5XHowIMb'
+
+access.token <- '804111108076228608-K6ml4zNsg2RZLb4ep3Dir4JJhuzb1AQ'
+access.secret <- 'nwvP4yzA6mKNE2P0Ma0diEvHQNN24McUnLH80kbmD4uGx'
+
+options(httr_oauth_cache=T) #This will enable the use of a local file to cache OAuth access credentials between R sessions.
+setup_twitter_oauth(consumer.key, consumer.secret, access.token, access.secret)
+
+
+
+########### Below is the stuff for favorited tweets ##############
+
+
+lucaspuente <- getUser("lucaspuente")
+location(lucaspuente)
+
+lucaspuente_follower_IDs <- lucaspuente$getFollowers(retryOnRateLimit=180)
+length(lucaspuente_follower_IDs)
+
+favs <- favorites("lucaspuente", n = 21)
+# strsplit(favs[[1]], ":")
+
+unlist(favs)
+View(favs)
+View(favs[[1]])
+name <- favs[[2]]$screenName
+location(getUser(favs[[1]]$screenName))
+location(getUser(favs[[6]]$screenName))
+
+ (getUser(favs[[1]]$screenName))$location
+
+####
+
+
+favs <- favorites("lucaspuente", n = 21)
+# strsplit(favs[[1]], ":")
+
+fav <- unlist(favs)
+View(favs)
+View(favs[[1]])
+name <- favs[[2]]$screenName
+location(getUser(favs[[1]]$screenName))
+location(getUser(favs[[6]]$screenName))
+
+(getUser(fav$screenName$location))
+
+
+
+
