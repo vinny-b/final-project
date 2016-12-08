9711a0c20e89d31a4f519880a5ab5bed547ffb82
kobukk <kskobuke@gmail.com>
2016-12-07 16:52:18 -0800

    idk

--- getFav.R
+++ getFav.R
@@ -25,106 +25,81 @@ setup_twitter_oauth(consumer.key, consumer.secret, access.token, access.secret)
 ########### Below is the stuff for favorited tweets ##############
 
 ####
+getFav <- function(inputHandle) {
+  #get favorites
+  
 
-#get favorites
-
-inputHandle <- "lucaspuente"
-
-favs <- favorites(inputHandle, n = 100)
-
-fav <- unlist(favs)
-
-
-#length(fav)
-
-## for loop to build list of locations
-locationlist <- c()
-for (i in 1:length(fav))
-  locationlist <- append(locationlist, (getUser(fav[[i]]$screenName)$location))
-
-###
-
-# remove everything before comma 
-
-cleanlist <- gsub(".*,", "", locationlist)
-
-
-#convert list to dataframe 
-dflist <- as.data.frame(cleanlist)
-
-#fix column name
-colnames(dflist) <- "stateabbrev"
-
-#dflist <- as.data.frame(dflist, stringsAsFactors = FALSE)
-# dflist <- droplevels(dflist)
-View(dflist)
-
-
-#for loop to keep only state abbrev
-
-#states <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
-#states <- as.data.frame(states, stringsAsFactors = FALSE)
-
-# for (i in 1:length(dflist)) {
-#   for (j in 1:length(states)) {
-#     if (!identical(dflist$stateabbrev[i], states[j])) {
-#       dflist <- dflist[, -c(i)]
-#     }
-#   }
-# }
+  favs <- favorites(inputHandle, n = 100)
+  
+  fav <- unlist(favs)
+  
+  
+  #length(fav)
+  
+  ## for loop to build list of locations
+  locationlist <- c()
+  for (i in 1:length(fav))
+    locationlist <- append(locationlist, (getUser(fav[[i]]$screenName)$location))
+  
+  ###
+  
+  # remove everything before comma 
+  
+  cleanlist <- gsub(".*,", "", locationlist)
+  
+  
+  #convert list to dataframe 
+  dflist <- as.data.frame(cleanlist)
+  
+  #fix column name
+  colnames(dflist) <- "stateabbrev"
+  
+  #dflist <- as.data.frame(dflist, stringsAsFactors = FALSE)
+  # dflist <- droplevels(dflist)
+  View(dflist)
+  
+  
+  
+  flatlist <- flatten(dflist)
+  
+  liststateab <- grep('AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY', flatlist$stateabbrev, value = TRUE)
+  df1 <- data.frame(liststateab)
+  
+  colnames(df1) <- "states1"
 
 
-flatlist <- flatten(dflist)
-
-liststateab <- grep('AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY', flatlist$stateabbrev, value = TRUE)
-df1 <- data.frame(liststateab)
-
-colnames(df1) <- "states1"
-
-View(df1)
-
-
-
-## practice with state codes DATA
-
-
-#df of states
-
-flatstate <- c("MA", "OR", "CA", "CA", "TX", "MD", "MD", "MD", "IL", "IL", "IL", "IL", "IL")
-datastate <- as.data.frame(flatstate)
-View(datastate)
-
-###
-
-
-#count frequency 
-exstate <- df1 %>% 
-  group_by(states1) %>%
-  summarize(count = n())
-
-
-
-#build frequency heat map
-
-# give state boundaries a white border
-l <- list(color = toRGB("white"), width = 2)
-# specify some map projection/options
-g <- list(
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showlakes = TRUE,
-  lakecolor = toRGB('white')
-)
-
-
-plot_geo(exstate, locationmode = 'USA-states') %>%
-  add_trace(
-    z = ~count, locations = ~states1,
-    color = ~count, colors = 'Purples'
-  ) %>%
-  colorbar(title = "Users") %>%
-  layout(
-    title = paste("Location of users that", inputHandle, "most recently favorited"),
-    geo = g
+  
+  
+  #count frequency 
+  exstate <- df1 %>% 
+    group_by(states1) %>%
+    summarize(count = n())
+  
+  
+  
+  #build frequency heat map
+  
+  # give state boundaries a white border
+  l <- list(color = toRGB("white"), width = 2)
+  # specify some map projection/options
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('white')
   )
+  
+  
+  map <- plot_geo(exstate, locationmode = 'USA-states') %>%
+    add_trace(
+      z = ~count, locations = ~states1,
+      color = ~count, colors = 'Purples'
+    ) %>%
+    colorbar(title = "Users") %>%
+    layout(
+      title = paste("Location of users that", inputHandle, "most recently favorited"),
+      geo = g
+    )
 
+  return (map)
+}
