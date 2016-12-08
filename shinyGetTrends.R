4c251fe97091d2b74d9b88ed209180136c0e66ca
Anish Velagapudi <16AnishV@gmail.com>
2016-12-07 17:00:37 -0800

    Now reading in csv file rather than storing all location data locally

--- shinyGetTrends.R
+++ shinyGetTrends.R
@@ -32,81 +32,15 @@ shinyGetTrends <- function (inputString, inputCity = "Albuquerque") {
   trends.data.frame <- trends.data.frame[-c(64), ]
   # View(trends.data.frame)
   
-  # Takes in a city name (from the list of major cities that Twitter has current trending
-  # data in) as a string and returns that location's WOEID (the Where On Earth Identifier)
-  # as a string.
-  location <- trends.data.frame %>% filter(name == inputCity)
-  woeids <- location$woeid
-  
-  # Key-Value pair list of all cities that twitteR has trending data on and their
-  # corresponding longitude and latitude coordinates
-  geo.location.data <- list(Albuquerque="35.085334,-106.605553",
-                            Atlanta="33.748995,-84.387982",
-                            Austin="30.267153,-97.743061",
-                            Baltimore="39.290385,-76.612189",
-                            Baton_Rouge="30.458283,-91.14032",
-                            Birmingham="33.520661,-86.80249",
-                            Boston="42.360082,-71.05888",
-                            Charlotte="35.227087,-80.843127",
-                            Chicago="41.878114,-87.629798",
-                            Cincinnati="39.103118,-84.51202",
-                            Cleveland="41.499320,-81.694361",
-                            Colorado_Springs="38.833882,-104.821363",
-                            Columbus="39.961176,-82.998794",
-                            Dallas_Ft._Worth="32.761422,-96.858013",
-                            Denver="39.739236,-104.990251",
-                            Detroit="42.331427,-83.045754",
-                            El_Paso="31.761878,-106.485022",
-                            Fresno="36.746842,-119.772587",
-                            Greensboro="36.072635,-79.791975",
-                            Harrisburg="40.273191,-76.886701",
-                            Honolulu="21.306944,-157.858333",
-                            Houston="29.760427,-95.369803",
-                            Indianapolis="39.768403,-86.158068",
-                            Jackson="32.298757,-90.18481",
-                            Jacksonville="30.332184,-81.655651",
-                            Kansas_City="39.099727,-94.578567",
-                            Las_Vegas="36.114707,-115.17285",
-                            Long_Beach="33.770050,-118.193739",
-                            Los_Angeles="34.052234,-118.243685",
-                            Louisville="38.252665,-85.758456",
-                            Memphis="35.149534,-90.04898",
-                            Mesa="33.415184,-111.831472",
-                            Miami="25.761680,-80.19179",
-                            Milwaukee="43.038902,-87.906474",
-                            Minneapolis="44.977753,-93.265011",
-                            Nashville="36.162664,-86.781602",
-                            New_Haven="41.308274,-72.927884",
-                            New_Orleans="29.951066,-90.071532",
-                            New_York="40.712784,-74.005941",
-                            Norfolk="36.850769,-76.285873",
-                            Oklahoma_City="35.467560,-97.516428",
-                            Omaha="41.252363,-95.997988",
-                            Orlando="28.538335,-81.379236",
-                            Philadelphia="39.952584,-75.165222",
-                            Phoenix="33.448377,-112.074037",
-                            Pittsburgh="40.440625,-79.995886",
-                            Portland="45.523062,-122.676482",
-                            Providence="41.823989,-71.412834",
-                            Raleigh="35.779590,-78.638179",
-                            Richmond="37.540725,-77.436048",
-                            Sacramento="38.581572,-121.4944",
-                            St._Louis="38.610302,-90.412518",
-                            Salt_Lake_City="40.760779,-111.891047",
-                            San_Antonio="29.424122,-98.493628",
-                            San_Diego="32.715738,-117.161084",
-                            San_Francisco="37.774929,-122.419416",
-                            San_Jose="37.338208,-121.886329",
-                            Seattle="47.606209,-122.332071",
-                            Tallahassee="30.438256,-84.280733",
-                            Tampa="27.950575,-82.457178",
-                            Tucson="32.221743,-110.926479",
-                            Virginia_Beach="36.852926,-75.977985",
-                            Washington="38.907192,-77.036871")
-  
+  # Dataframe of all cities that twitteR has trending data on and their
+  # corresponding longitude and latitude coordinates. This list of cities
+  # was retrieved by using twitteR's availableTrendLocations() method
+  geo.location.data <- read.csv(file = 'GeoLocationData.csv')
+  View(geo.location.data)
+
   
   # Formats and returns the user's search string so that it's compatible with the 
-  # twitteR search functions
+  # twitteR searchTwitter function that retrieves the posts
   formatted.search.string <- gsub(" ", "+", inputString)
   
   # Returns the longitude and latitude of the given city that twitteR has trending
