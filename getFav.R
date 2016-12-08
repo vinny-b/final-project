0ca4ef6cd02f0001213cbe0b8fcb25d96d5cc963
Vinny <vinayakbansal@hotmail.com>
2016-12-07 23:51:54 -0800

    Changes

--- getFav.R
+++ getFav.R
@@ -1,4 +1,9 @@
-# install.packages('twitteR')
+#installing packages
+
+#install.packages('twitteR')
+
+#Loading packages
+
 library(twitteR)
 library(dplyr)
 library(plotly)
@@ -13,9 +18,11 @@ if (!require("twitteR")) {
   library("twitteR")
 }
 
+#Storing the consumer key and variable
 consumer.key <- '974S5E5vr0pJ8VuOx2oIYf4Z0'
 consumer.secret <- '8acGBD7TKoLhFVG5TSsfa0PMeUpwnQgQVDwL1BfoGy5XHowIMb'
 
+#Storing the access token and secret
 access.token <- '804111108076228608-K6ml4zNsg2RZLb4ep3Dir4JJhuzb1AQ'
 access.secret <- 'nwvP4yzA6mKNE2P0Ma0diEvHQNN24McUnLH80kbmD4uGx'
 
@@ -24,11 +31,11 @@ setup_twitter_oauth(consumer.key, consumer.secret, access.token, access.secret)
 
 
 
-########### Below is the stuff for favorited tweets ##############
+########### The favorited tweets are extracted below ##############
 
-####
+#### 
 
-#get favorites
+#getting favorites of the user handle
 getFav <- function(inputHandle){
 favs <- favorites(inputHandle, n = 100)
 fav <- unlist(favs)
@@ -41,34 +48,34 @@ for (i in 1:length(fav))
   locationlist <- append(locationlist, (getUser(fav[[i]]$screenName)$location))
 
 
-# remove everything before comma 
+# removing everything before comma in the lise
 listaftercomma <- gsub(".*,", "", locationlist)
 
-#convert list to dataframe 
+#converting list to dataframe 
 dflist <- as.data.frame(listaftercomma)
 
-#fix column name
+#Changing column name
 colnames(dflist) <- "stateabbrev"
 
 
-#remove all locations that aren't in state abbreviation format 
+#Removing all locations that aren't in state abbreviation format 
 liststateab <- grep('AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY', dflist$stateabbrev, value = TRUE)
 
 
 
 
-# build a clean list of the data
+# Building a clean list of the data
 cleanlist <- c()
 for (i in 1:length(liststateab))
   cleanlist <- append(cleanlist, as.character(liststateab[i]))
 
-#remove all spaces
+#Removing all spaces
 statesDataFrame <- gsub('\\s+', '', cleanlist)
 
-#make into a dataframe 
+#Converting into dataframe 
 clean_df <- as.data.frame(statesDataFrame)
 
-# update column name 
+#Changing column name 
 colnames(clean_df) <- "states"
 
 
@@ -77,18 +84,18 @@ colnames(clean_df) <- "states"
 ###
 
 
-#count frequency 
+#Using dplyr to summarize the data 
 exstate <- clean_df %>% 
   group_by(states) %>%
   summarize(count = n())
 
 
 
-#build frequency heat map
+#Building frequency heat map
 
-# give state boundaries a white border
+#Giving state boundaries a white border
 l <- list(color = toRGB("white"), width = 2)
-# specify some map projection/options
+#Specifying some map attributes
 g <- list(
   scope = 'usa',
   projection = list(type = 'albers usa'),
@@ -96,17 +103,19 @@ g <- list(
   lakecolor = toRGB('white')
 )
 
-
+#Specifying Columns and Axis
 map <- plot_geo(exstate, locationmode = 'USA-states') %>%
   add_trace(
     z = ~count, locations = ~states,
     color = ~count, colors = 'Oranges'
   ) %>%
+#Setting the colorbar   
   colorbar(title = "Users") %>%
   layout(
     title = paste("Location of users that", inputHandle, "most recently favorited"),
     geo = g
   )
+#Returning map
 return(map)
 }
 
