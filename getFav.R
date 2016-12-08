caaecc0eab67787c27d1d92020285dd9443cc427
kobukk <kskobuke@gmail.com>
2016-12-07 17:04:26 -0800

    fixed problem

--- getFav.R
+++ getFav.R
@@ -6,6 +6,8 @@ library(maps)
 library(jsonlite)
 
 
+
+
 if (!require("twitteR")) {
   install.packages("twitteR", repos="http://cran.rstudio.com/") 
   library("twitteR")
@@ -25,81 +27,84 @@ setup_twitter_oauth(consumer.key, consumer.secret, access.token, access.secret)
 ########### Below is the stuff for favorited tweets ##############
 
 ####
-getFav <- function(inputHandle) {
-  #get favorites
-  
 
-  favs <- favorites(inputHandle, n = 100)
-  
-  fav <- unlist(favs)
-  
-  
-  #length(fav)
-  
-  ## for loop to build list of locations
-  locationlist <- c()
-  for (i in 1:length(fav))
-    locationlist <- append(locationlist, (getUser(fav[[i]]$screenName)$location))
-  
-  ###
-  
-  # remove everything before comma 
-  
-  cleanlist <- gsub(".*,", "", locationlist)
-  
-  
-  #convert list to dataframe 
-  dflist <- as.data.frame(cleanlist)
-  
-  #fix column name
-  colnames(dflist) <- "stateabbrev"
-  
-  #dflist <- as.data.frame(dflist, stringsAsFactors = FALSE)
-  # dflist <- droplevels(dflist)
-  View(dflist)
-  
-  
-  
-  flatlist <- flatten(dflist)
-  
-  liststateab <- grep('AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY', flatlist$stateabbrev, value = TRUE)
-  df1 <- data.frame(liststateab)
-  
-  colnames(df1) <- "states1"
+#get favorites
+
+favs <- favorites(inputHandle, n = 100)
+fav <- unlist(favs)
 
 
-  
-  
-  #count frequency 
-  exstate <- df1 %>% 
-    group_by(states1) %>%
-    summarize(count = n())
-  
-  
-  
-  #build frequency heat map
-  
-  # give state boundaries a white border
-  l <- list(color = toRGB("white"), width = 2)
-  # specify some map projection/options
-  g <- list(
-    scope = 'usa',
-    projection = list(type = 'albers usa'),
-    showlakes = TRUE,
-    lakecolor = toRGB('white')
+
+## for loop to build list of locations
+locationlist <- c()
+for (i in 1:length(fav))
+  locationlist <- append(locationlist, (getUser(fav[[i]]$screenName)$location))
+
+
+# remove everything before comma 
+listaftercomma <- gsub(".*,", "", locationlist)
+
+#convert list to dataframe 
+dflist <- as.data.frame(listaftercomma)
+
+#fix column name
+colnames(dflist) <- "stateabbrev"
+
+
+#remove all locations that aren't in state abbreviation format 
+liststateab <- grep('AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY', dflist$stateabbrev, value = TRUE)
+df1 <- as.data.frame(liststateab)
+
+
+
+# build a clean list of the data
+cleanlist <- c()
+for (i in 1:length(liststateab))
+  cleanlist <- append(cleanlist, as.character(liststateab[i]))
+
+#remove all spaces
+statesDataFrame <- gsub('\\s+', '', cleanlist)
+
+#make into a dataframe 
+clean_df <- as.data.frame(statesDataFrame)
+
+# update column name 
+colnames(clean_df) <- "states"
+
+
+
+
+###
+
+
+#count frequency 
+exstate <- clean_df %>% 
+  group_by(states) %>%
+  summarize(count = n())
+
+
+
+#build frequency heat map
+
+# give state boundaries a white border
+l <- list(color = toRGB("white"), width = 2)
+# specify some map projection/options
+g <- list(
+  scope = 'usa',
+  projection = list(type = 'albers usa'),
+  showlakes = TRUE,
+  lakecolor = toRGB('white')
+)
+
+
+plot_geo(exstate, locationmode = 'USA-states') %>%
+  add_trace(
+    z = ~count, locations = ~states,
+    color = ~count, colors = 'Oranges'
+  ) %>%
+  colorbar(title = "Users") %>%
+  layout(
+    title = paste("Location of users that", inputHandle, "most recently favorited"),
+    geo = g
   )
-  
-  
-  map <- plot_geo(exstate, locationmode = 'USA-states') %>%
-    add_trace(
-      z = ~count, locations = ~states1,
-      color = ~count, colors = 'Purples'
-    ) %>%
-    colorbar(title = "Users") %>%
-    layout(
-      title = paste("Location of users that", inputHandle, "most recently favorited"),
-      geo = g
-    )
 
-  return (map)
-}
